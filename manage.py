a914814700cfaf28f1df556f45aab202d4398cce
Anuj Patel <anujpatel@berkeley.edu>
2016-02-16 20:50:41 -0800

    manage.py working??

--- manage.py
+++ manage.py
@@ -2,12 +2,10 @@
 import os, sys
 
 from flask import Flask
-from flaskext.actions import Manager
 import settings
 from fuckr import app
 
 app.config.from_object(settings)
-manager = Manager(app)
 
 if __name__ == '__main__':
-	manager.run()
+	app.run()
